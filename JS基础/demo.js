d2ca1a072248393805f23e8ecc4bd4e6730bceae
SupercuteYX <55016074+SupercuteYX@users.noreply.github.com>
2021-07-02 11:48:17 +0800

    Add files via upload

---
+++ JS基础/demo.js
@@ -0,0 +1,17 @@
+const a = [2, 7, 5, 4, 5]
+const b = ["h", "e", "l", "l", "o"]
+
+// c = [
+// {2 : "h"}
+// {7 : "e"}
+// {5 : "l"}
+// {4 : "l"}
+// {5 : "o"}
+// ]
+//const c = [];
+//a.forEach((element, index) => c.push({[element] : b[index]}));
+//console.log(c)
+
+//const d = a.map((element,index) => ({[element]:b[index]}))
+//console.log(d)
+
